c318c6ab6afcfcf00d9fde6751256ebcd7410516
evgeniy <ksabre86@gmail.com>
2022-04-18 00:32:13 +0500

    первая программа редакт.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -10,17 +10,12 @@ let personalMovieDB = {
     privat: false,
 };
 
-let questionLastFilm_1 = prompt('Один из последних просмотренных фильмов', '');
-let questionRating_1 = prompt('На сколько оцените его?', '');
-let questionLastFilm_2 = prompt('Один из последних просмотренных фильмов', '');
-let questionRating_2 = prompt('На сколько оцените его?', '');
+const questionLastFilm_1 = prompt('Один из последних просмотренных фильмов', '');
+const questionRating_1 = prompt('На сколько оцените его?', '');
+const questionLastFilm_2 = prompt('Один из последних просмотренных фильмов', '');
+const questionRating_2 = prompt('На сколько оцените его?', '');
 
-let movies = {
-    first1: questionLastFilm_1,
-    rating1: questionRating_1,
-    first2: questionLastFilm_2,
-    rating2: questionRating_2,
-};
+personalMovieDB.movies[questionLastFilm_1] = questionRating_1;
+personalMovieDB.movies[questionLastFilm_2] = questionRating_2;
 
 console.log(personalMovieDB);
-console.log(movies);
