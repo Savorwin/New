017d53fcedd30b7cf67be977a2b5cb059c8d2249
evgeniy <ksabre86@gmail.com>
2022-04-18 00:22:37 +0500

    первая программа

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 "use strict";
 
-console.log('Hello world!');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let questionLastFilm_1 = prompt('Один из последних просмотренных фильмов', '');
+let questionRating_1 = prompt('На сколько оцените его?', '');
+let questionLastFilm_2 = prompt('Один из последних просмотренных фильмов', '');
+let questionRating_2 = prompt('На сколько оцените его?', '');
+
+let movies = {
+    first1: questionLastFilm_1,
+    rating1: questionRating_1,
+    first2: questionLastFilm_2,
+    rating2: questionRating_2,
+};
+
+console.log(personalMovieDB);
+console.log(movies);
