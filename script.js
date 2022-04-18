180139db374e5643aeb8e068a5f99dc80ba2c2b9
evgeniy <ksabre86@gmail.com>
2022-04-18 19:09:09 +0500

    сложная практика

--- script.js
+++ script.js
@@ -1,48 +1,33 @@
 "use strict";
 
-for (let i = 5; i <= 10; i++) {
-    console.log(i);
+const arr = [3, 5, 8, 16, 20, 23, 50];
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
 }
+console.log(result);
 
 
-for (let i = 20; i >= 10; i--) {
-    if (i === 13) {
-        break;
+let data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === "number") {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i]) === "string") {
+        data[i] = data[i] + " - done";
     }
-    console.log(i);
 }
 
-for (let i = 2; i <= 10; i++) {
-    if (i % 2 != 1) {
-        console.log(i);
-    }
+console.log(data);
+
+
+const data1 = [5, 10, 'Shopping', 20, 'Homework'];
+const result1 = [];
+
+for (let i = 0; i <= data1.length; i++) {
+    result1[i - 1] = data1[data1.length - i]; 
 }
 
-for (let i = 2; i <= 16; i++) {
-if (i % 2 === 0) {
-continue;
-} else {
-console.log(i);
-}
-};
+console.log(result);
 
-
-let i = 2;
-
-while (i <= 16) {
-    if (i % 2 === 0) {
-        i++;
-        continue;
-    } else {
-        console.log(i);
-    }
-    i++;
-}
-
-
-let arr = [];
-for (let i = 5; i <= 10; i++) {
-    arr[i - 5] = i;
-}
-
-console.log(arr);
