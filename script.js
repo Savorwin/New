c6386a9e2bc713ba76fe9badf5d5dd89cc46f49f
evgeniy <ksabre86@gmail.com>
2022-04-20 13:35:09 +0500

    аргументы функций

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
 "use strict";
 
-let num = 20;
+const usdCurr = 28;
+const eurCurr = 32;
 
-function showFirstMessage(text) {
-    console.log(text);
-    num = 10;
-}; 
-
-showFirstMessage("Hello world");
-console.log(num);
-
-
-function calc(a, b) {
-    return a + b;
+function convert(amount, curr) {
+    console.log(curr * amount);
 };
 
-console.log(calc(5, 4));
-console.log(calc(6, 6));
-console.log(calc(3, 7));
-
-function ret() {
-    let num = 50;
-    return num;
-};
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-    console.log("Hello");
-};
-
-logger();
-
-
-const calc1 = (a, b) => a + b;
-calc1();
+convert(500, usdCurr);
+convert(500, eurCurr);
