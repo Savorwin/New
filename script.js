e4086802074e809d97057854162cc9a6c1a8424a
evgeniy <ksabre86@gmail.com>
2022-04-20 13:55:38 +0500

    return

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 "use strict";
 
 const usdCurr = 28;
-const eurCurr = 32;
+const discount = 0.9;
 
 function convert(amount, curr) {
-    console.log(curr * amount);
+    return curr * amount;
 };
 
-convert(500, usdCurr);
-convert(500, eurCurr);
+function promotion(result) {
+    console.log(result * discount);
+};
+
+promotion(convert(500, usdCurr));
+
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return;
+    }
+    console.log("Done");
+};
+
+test();
+
+function doNothing() {
+    console.log(doNothing() === undefined);
+};
