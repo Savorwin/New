310fcd6182942010b24283784e358b075427f9ec
evgeniy <ksabre86@gmail.com>
2022-04-22 15:01:23 +0500

    Fibo

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
 "use strict";
 
-function getTimeFromMinutes(timerMinutes) {
-    if (timerMinutes < 0 || typeof(timerMinutes) !== "number" || timerMinutes !== Math.round(timerMinutes)) {
-        return console.log("Ошибка, проверьте данные");
-    }
-    
-    let hours = Math.floor(timerMinutes / 60);
-    let minuts = timerMinutes % 60;
-    let hoursStr = "";
-
-    switch(hours) {
-        case 0: 
-            hoursStr = "часов";
-            break;
-        case 1: 
-            hoursStr = "час";
-            break;
-        case 2: 
-        case 3: 
-        case 4: 
-            hoursStr = "часа";
-            break;
-        default: 
-            hoursStr = "часов";  
+function fib(num) {
+    if(typeof(num) !== "number" || num < 0 || num !== Math.round(num)) {
+        return "";
     }
 
-    return console.log(`Это ${hours} ${hoursStr} и ${minuts} минут`);
+    let str = "";
+    let first = 0;
+    let second = 1;
+
+    for(let i = 0; i < num; i++) {
+        if (i + 1 === num) {
+            str += `${first}`;
+        } else {
+            str += `${first} `;
+        }
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return str;
 }
 
-getTimeFromMinutes(150);
+console.log(fib(5));
 
 
-function findMaxNumber(a, b, c, d) {
-    if (typeof(a) !== "number" || typeof(b) !== "number" || typeof(c) !== "number" || typeof(d) !== "number") {
-        return 0;
-    }
 
-    if (a > b && a > c && a > d) {
-        return a;
-    } else if (b > a && b > c && b > d) {
-        return b;
-    } else if (c > a && c > b && c > d) {
-        return c;
-    } else return d;
+//0 + 1 = 1, 
+//1 + 1 = 2,
+//1 + 2 = 3.
 
-}
-
-console.log(findMaxNumber(11, 10, 7, 9));
