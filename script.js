983d7089cdaf95a8b3abe7308431a926c7fec15f
evgeniy <ksabre86@gmail.com>
2022-04-24 20:39:05 +0500

    Прототипы

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 "use strict";
 
-const someString = 'This is some strange string';
+let str = "some";
+let strObj = new String(str);
 
-function reverse(str) {
-    if(typeof(str) === "string") {
-    let reverse = str.split('').reverse().join('');
-    return reverse;
-    } else return "Ошибка!";
-}
-reverse(someString);
+console.log(typeof(str));
+console.log(typeof(strObj));
 
+console.dir([1, 2, 3]);
 
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-
-function availableCurr(arr, missingCurr) {
-    let str = '';
-    arr.length === 0 ? str = 'Нет доступных валют' : 'Доступные валюты: \n';
-
-    arr.forEach(function(item) {
-        if (item !== missingCurr) {
-        str += `${item}\n`;
-        }
-    });
-        return str;
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
     }
+};
 
-availableCurr([...baseCurrencies, ...additionalCurrencies]);
+const john = {
+    health: 100,
+};
+
+//john._proto_ = soldier;
+
+////////////// Новые методы прототипов
+
+Object.setPrototypeOf(john, soldier);
+
+john.sayHello();
+
+const john = Object.create(soldier);
