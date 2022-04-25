ed0343a9dcd300a50f572107d749ffc52bfd25f3
evgeniy <ksabre86@gmail.com>
2022-04-25 14:48:07 +0500

    практика кино изменение архитектуры

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
 "use strict";
 
-let str = "some";
-let strObj = new String(str);
+let personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            let lastFilm = prompt('Один из последних просмотренных фильмов', '').trim();
+            let rating = prompt('На сколько оцените его?', '');
+            if (lastFilm != "" && rating != "" && lastFilm != null && rating != null && lastFilm.length < 50) {  
+                personalMovieDB.movies[lastFilm] = rating;
+                console.log('done');
+                } else {
+                    console.log("error");
+                    i--;
+                }        
+    }
+    },
+    detectedPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            alert("Вы классический зритель");
+        } else if (personalMovieDB.count >= 31) {
+            alert("Вы киноман");
+        } else {
+            alert("Произошла ошибка");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (hidden === "false") {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-console.log(typeof(str));
-console.log(typeof(strObj));
-
-console.dir([1, 2, 3]);
-
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log("Hello");
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+            personalMovieDB.genres.forEach((item, i) => {
+                console.log(`Любимый жанр ${i + 1} - это ${item}`);
+            });
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
     }
 };
-
-const john = {
-    health: 100,
-};
-
-//john._proto_ = soldier;
-
-////////////// Новые методы прототипов
-
-Object.setPrototypeOf(john, soldier);
-
-john.sayHello();
-
-const john = Object.create(soldier);
