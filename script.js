3e47d00448f5f60a3e8f13e03538cfe5ef4e3887
evgeniy <ksabre86@gmail.com>
2022-04-26 13:52:32 +0500

    замыкания

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
 "use strict";
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-};
+function createCounter() {
+    let counter = 0;
 
+    const myFunction = function() {
+        counter = counter + 1;
+        return counter;
+    };
 
-function isBudgetEnough(data) {
-    let square = 0;
-    let value = 0;
-
-    data.shops.forEach(shop => {
-        square += shop.width * shop.length;
-        return square;
-    });
-
-    value = square * data.height;
-
-    if((value * data.moneyPer1m3) < data.budget) {
-        "Бюджета достаточно";
-    } else {
-        "Бюджета недостаточно";
-    }
+    return myFunction;
 }
 
-isBudgetEnough(shoppingMallData);
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
 
-
-
+console.log(c1, c2, c3);
