2c5037c39b9824c852dbf033e31647586c322d84
evgeniy <ksabre86@gmail.com>
2022-04-27 00:02:46 +0500

    действия с элементами на странице

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 "use strict";
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'), 
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
 
-console.log(box);
+box.style.backgroundColor = "blue";
+box.style.width = "500px";
 
-const btns = document.getElementsByTagName('button')[1];
+box.style.cssText = "background-color: blue; width: 500px";
 
-console.log(btns);
+btns[1].style.borderRadius = "100%";
 
-const circles = document.getElementsByClassName('circle');
+circles[0].style.backgroundColor = "red";
 
-console.log(circles);
-
-//более современные методы
-
-const hearts = document.querySelectorAll('.heart'); 
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.backgroundColor = "orange";
+}
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = "yellow";
 });
 
-const buttonOne = document.querySelector('button');
+//Методы для работы с элементами страницы
 
-console.log(buttonOne);
+const div = document.createElement('div');
+div.classList('black');
+//const text = document.createTextNode('Тут был я');
+
+//document.body.append(div);
+
+//document.querySelector('.wrapper').append(div);
+
+//wrapper.prepend(div);
+
+//hearts[0].before(div);
+//hearts[0].after(div);
+
+//circles[0].remove();
+
+//hearts[0].replaceWith(circles[0]);
+
+div.innerHTML = "Hello world";
+div.textContent = "Hello world";
