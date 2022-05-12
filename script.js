fd4ae98494ff13b39b24bbe43984c7bbefb911a0
evgeniy <ksabre86@gmail.com>
2022-05-12 23:13:40 +0500

    set

--- script.js
+++ script.js
@@ -1,68 +1,18 @@
 "use strict";
 
-const user = {
-    name: "Alex",
-    surname: "Smith",
-    birthday: "20/04/1993",
-    showMyPublicData: function() {
-        console.log(`${this.name} ${this.surname}`);
-    }
-};
+const arr = [1, 1, 2, 2, 4, 5, 6, 5];
 
-console.log(user);
+const set = new Set(arr);
 
-//map
+console.log(set);
 
-const shops = [
-    {rice: 500},
-    {oil: 200},
-    {bread: 50},
-];
+//функция помощник
 
-const map = new Map([
-    [{paper: 400}, 8000]
-]); // создание пустой карты
+const arr2 = ['Alex', 'Ann', 'Oleg', 'Alex'];
 
-map.set(shops[0], 5000)
-    .set(shops[1], 15000)
-    .set(shops[2], 25000);
-
-console.log(map);
-
-//перебор map
-
-//1
-for (let shop of map.keys()) {
-    console.log(shop);
+function unique(arr2) {
+    return Array.from(new Set(arr2));
 }
 
-const goods = [];
-for(let shop of map.keys()){
-    goods.push(Object.keys(shop)[0]);
-}
-console.log(goods);
+console.log(unique(arr2));
 
-//2
-for (let price of map.values()) {
-    console.log(price);
-}
-
-//3
-for (let price of map.entries()) {
-    console.log(price);
-}
-
-//4
-map.forEach( (value, keys, map) => {
-    console.log(value, keys);
-});
-
-//Создать map из объекта
-
-const userMap = new Map(Object.entries(user));
-console.log(userMap);
-
-//Создать из map объект
-
-const newUserObj = Object.fromEntries(userMap);
-console.log(newUserObj);
