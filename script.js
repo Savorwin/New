1cc57d7d8de2e1a9142e4f854ada3934c1e00ad5
evgeniy <ksabre86@gmail.com>
2022-05-18 14:05:48 +0500

    практика, таймер на странице

--- script.js
+++ script.js
@@ -1,25 +1,100 @@
 "use strict";
 
-//const now = new Date('2022-06-17');
-//new Date.parse('2022-06-17');
+window.addEventListener('DOMContentLoaded', function() {
 
-/* console.log(now.getFullYear()); // получить год
-console.log(now.getMonth()); // получить месяц
-console.log(now.getDate()); // получить день
-console.log(now.getHours()); // получить часы
-console.log(now.getMinutes()); // получить минуты
-console.log(now.getSeconds()); // получить секунды
-console.log(now.getDay()); // получить день недели
-console.log(now.getUTCHours()); // получить часы по UTC */
+    // Tabs
+    
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
-// установить время
-//console.log(now.setHours(18)); // установить часы
+	function hideTabContent() {
+        
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-let start = new Date();
-for( let i = 0; i < 100000; i++) {
-    let some = i ** 3;
-}
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+	}
 
-let end = new Date();
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    
+    hideTabContent();
+    showTabContent();
 
-alert(`Цикл отработал за ${end - start} миллисекунд`);
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+    });
+
+    //Timer
+
+    const deadline = '2022-05-25'; //отправная точка
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            "total": t,
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds,
+        };
+
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds"),
+              timeInterval = setInterval(updateClock, 1000);
+
+              updateClock();
+        
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+});
+
