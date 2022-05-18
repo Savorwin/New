8a7150aa3d0ac052b975f8d7bfa905fb3230d12a
evgeniy <ksabre86@gmail.com>
2022-05-18 14:13:14 +0500

    обработка прошедшей даты

--- script.js
+++ script.js
@@ -43,22 +43,32 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Timer
 
-    const deadline = '2022-05-25'; //отправная точка
+    const deadline = '2022-05-19'; //отправная точка
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+        else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
               hours = Math.floor((t / (1000 * 60 * 60) % 24)),
               minutes = Math.floor((t / 1000 / 60) % 60),
               seconds = Math.floor((t / 1000) % 60);
+        }
 
         return {
             "total": t,
             "days": days,
             "hours": hours,
             "minutes": minutes,
-            "seconds": seconds,
-        };
+            "seconds": seconds,    
+    };
 
     }
 
