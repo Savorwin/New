af9f361217facf5dd1da1a8af2d1d52673933d7b
evgeniy <ksabre86@gmail.com>
2022-05-24 18:13:12 +0500

    практика методы массивов 2

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
 "use strict";
 
-const films = [
-    {
-        name: 'Titanic',
-        rating: 9
-    },
-    {
-        name: 'Die hard 5',
-        rating: 5
-    },
-    {
-        name: 'Matrix',
-        rating: 8
-    },
-    {
-        name: 'Some bad film',
-        rating: 4
-    }
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
 ];
 
-function showGoodFilms(films) {
-    return films.filter(film => film.rating >= 8);
-}
-console.log(showGoodFilms(films));
+const getPositiveIncomeAmount = (data) => {
+    return data.filter(item => item.amount > 0)
+    .reduce((sum, current) => sum + current.amount);
+};
 
-function showListOfFilms(films) {
-    return films.reduce((film, current) => `${typeof(film) === 'object' ? film.name : film}, ${current.name}`);
-}
-console.log(showListOfFilms(films));
+console.log(getPositiveIncomeAmount(funds));
 
-function setFilmsIds(films) {
-    films.map(function(film, i){
-        film.id = i;
-        return film;
-    });
-}
-const tranformedArray = setFilmsIds(films);
 
-function checkFilms(tranformedArray) {
-    return tranformedArray.every(film => film.id || film.id === 0);
-}
 
-checkFilms(tranformedArray);
+
+
+const getTotalIncomeAmount = (data) => {
+
+};
+
+getTotalIncomeAmount(funds);
