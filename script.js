bb7e29e7e1f33a6b0c2aa677dae539fc43b5359b
evgeniy <ksabre86@gmail.com>
2022-05-24 17:41:42 +0500

    практика методы массивов

--- script.js
+++ script.js
@@ -1,66 +1,44 @@
 "use strict";
 
-//filter
-
-const names = [
-    'Ivan',
-    'Dmitry',
-    'Ksenia',
-    'Ann',
-    'Voldemart'
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
 ];
 
-const shortNames = names.filter(function(name){
-    return name.length < 5;
-});
+function showGoodFilms(films) {
+    return films.filter(film => film.rating >= 8);
+}
+console.log(showGoodFilms(films));
 
-console.log(shortNames);
+function showListOfFilms(films) {
+    return films.reduce((film, current) => `${typeof(film) === 'object' ? film.name : film}, ${current.name}`);
+}
+console.log(showListOfFilms(films));
 
-//map
+function setFilmsIds(films) {
+    films.map(function(film, i){
+        film.id = i;
+        return film;
+    });
+}
+const tranformedArray = setFilmsIds(films);
 
-const answers = ['IvAn', 'AnnA', 'Hello'];
+function checkFilms(tranformedArray) {
+    return tranformedArray.every(film => film.id || film.id === 0);
+}
 
-const result = answers.map(item => {
-    return item.toLowerCase();
-});
-
-console.log(result);
-
-//every/some
-
-const someTest = [4, 'twet', 'hdfhdfh'];
-
-console.log(someTest.some(item => typeof(item) === 'number'));
-
-const num = [4, 5, 7];
-
-console.log(num.every(item => typeof(item) === 'number'));
-
-//reduce
-
-/* const arr = [4, 5, 1, 3, 2, 6];
-
-const res = arr.reduce((sum, current) => sum + current);
-
-console.log(res); */
-
-const arr = ['apple', 'pear', 'plum'];
-
-const res = arr.reduce((sum, current) => `${sum}, ${current}`);
-
-console.log(res);
-
-//пример использования
-
-const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal'
-};
-
-const newArr = Object.entries(obj) // вернулся массив
-.filter(item => item[1] === 'persone')
-.map(item => item[0]); 
-
-console.log(newArr);
+checkFilms(tranformedArray);
