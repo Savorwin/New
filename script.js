75e83677822ea5b63f180c1290989864b982033c
evgeniy <ksabre86@gmail.com>
2022-06-07 18:01:45 +0500

    макро и микрозадачи

--- script.js
+++ script.js
@@ -1,73 +1,17 @@
 "use strict";
 
-const btnPhone = document.querySelector('#iphone'),
-      btnMacbook = document.querySelector('#macbook'),
-      images = document.querySelectorAll('img');
+setTimeout(() => {
+    console.log('timeout');
+}, 0);
 
-/* const phoneAnimation = images[0].animate([
-    {transform: 'translateY(0)'},
-    {transform: 'translateY(100px)'},
-    {transform: 'translateY(-100px)'},
-    {transform: 'translateY(0)'}
-], {
-    duration: 3000,
-    iterations: Infinity
-}); */
+Promise.resolve()
+    .then(() => console.log('promise'));
 
-let phoneAnimation;
-btnPhone.addEventListener('click', () => {
-    if(!phoneAnimation) {
-        phoneAnimation = images[0].animate([
-            {transform: 'translateY(0) rotate(0deg)',
-             filter: 'opacity(100%)'
-            },
-            {transform: 'translateY(100px) rotate(180deg)',
-            filter: 'opacity(50%)'
-            },
-            {transform: 'translateY(-100px) rotate(270deg)',
-            filter: 'opacity(75%)'
-            },
-            {transform: 'translateY(0) rotate(360deg)',
-            filter: 'opacity(100%)'
-            }
-        ], {
-            duration: 3000,
-            iterations: Infinity
-        });
-    } else if (phoneAnimation.playState === 'paused') {
-        phoneAnimation.play();
-    } else {
-        phoneAnimation.pause();
-    }
-});
+    queueMicrotask(() => console.log('wow'));
 
-let macbookAnimation;
-btnMacbook.addEventListener('click', () => {
-    if (!macbookAnimation) {
-        macbookAnimation = images[1].animate([
-            {transform: 'translateX(0) translateY(0)',
-            filter: 'opacity(100%)'
-            },
-            {transform: 'translateX(50px) translateY(50px)',
-            filter: 'opacity(80%)'
-            },
-            {transform: 'translateX(100px) translateY(100px)',
-            filter: 'opacity(60%)'
-            },
-            {transform: 'translateX(-50px) translateY(-50px)',
-            filter: 'opacity(80%)'
-            },
-            {transform: 'translateX(-100px) translateY(-100px)',
-            filter: 'opacity(100%)'
-            },
-            {transform: 'translateX(0) translateY(0)'}
-        ], {
-            duration: 3000,
-            iterations: Infinity
-        });
-    } else if (macbookAnimation.playState === 'paused') {
-    macbookAnimation.play(); 
-} else {
-    macbookAnimation.pause();
-}
-});
+    Promise.resolve()
+    .then(() => console.log('promise_2'));
+
+console.log('code');
+
+
